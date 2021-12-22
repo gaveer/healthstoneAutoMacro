63bf135ad2bb33bf03d357ecfc2848044b7220e4
gaveer <45834601+gaveer@users.noreply.github.com>
2021-12-22 13:39:05 +0300

    Update code.lua

--- code.lua
+++ code.lua
@@ -133,7 +133,8 @@ do
           end
         end
       end
-      macroStr = "#showtooltip \n/castsequence reset=" .. resetType .. " " .. potsString;
+      macroStr = "#showtooltip \n/cast " .. potsString;
+      --macroStr = "#showtooltip \n/castsequence reset=" .. resetType .. " " .. potsString;
       EditMacro("HAMHealthPot", "HAMHealthPot", nil, macroStr, 1, nil)
     end
   end)
